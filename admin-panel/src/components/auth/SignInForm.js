b5b5dc2d052809237134e433f7fde5fe02f4b595
mtsymlov <mtsymlov@yandex.ru>
2017-12-10 20:14:17 +0300

    HT1.3

--- admin-panel/src/components/auth/SignInForm.js
+++ admin-panel/src/components/auth/SignInForm.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react'
 import {reduxForm, Field} from 'redux-form'
 
+import validator from 'email-validator'
+import ErrorField from '../common/ErrorField'
+
 class SignInForm extends Component {
     static propTypes = {
 
@@ -12,10 +15,10 @@ class SignInForm extends Component {
                 <h3>Sign In</h3>
                 <form onSubmit={this.props.handleSubmit}>
                     <div>
-                        email: <Field name='email' component='input'/>
+                        email: <Field name='email' component={ErrorField} />
                     </div>
                     <div>
-                        password: <Field name='password' component='input' type='password'/>
+                        password: <Field name='password' component={ErrorField} type='password'/>
                     </div>
                     <input type='submit' />
                 </form>
@@ -25,6 +28,19 @@ class SignInForm extends Component {
     }
 }
 
+const validate = ({ email, password }) => {
+    const errors = {}
+
+    if (!email) errors.email = 'email is a required field'
+    if (email && !validator.validate(email)) errors.email = 'incorrect email format'
+
+    if (!password) errors.password = 'password is a required field'
+    if (password && password.length < 8) errors.password = 'password is to short'
+
+    return errors
+}
+
 export default reduxForm({
-    form: 'auth'
+    form: 'auth',
+    validate
 })(SignInForm)
