0bac3e87d34f7c336034b8bcdab6aea3144a2bec
Roman Iakobchuk <kaboom.ua@gmail.com>
2017-12-11 20:06:30 +0200

    add HT2

--- admin-panel/src/index.js
+++ admin-panel/src/index.js
@@ -2,5 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './config'
 import Root from './Root'
+import './mocks'
 
 ReactDOM.render(<Root />, document.getElementById('root'))
--- admin-panel/src/mocks/index.js
+++ admin-panel/src/mocks/index.js
@@ -5,3 +5,5 @@ export function saveEventsToFB() {
     const eventsRef = firebase.database().ref('/events')
     conferences.forEach(conference  => eventsRef.push(conference))
 }
+
+window.saveEventsToFB = saveEventsToFB
