0be404e032f6133196e4e114318e6bf7b7a0d967
Panchenko Svetlana <ivanovasvetlana1991@gmail.com>
2017-12-13 02:41:25 +0400

    HT2.1
    Merge branch 'master' of https://github.com/romabelka/advreact_04_12
    
    # Conflicts:
    #	admin-panel/src/App.js
    #	admin-panel/src/ducks/auth.js
    #	admin-panel/src/redux/reducer.js

--- admin-panel/src/config.js
+++ admin-panel/src/config.js
@@ -3,6 +3,7 @@ import firebase from 'firebase'
 export const appName = 'advreact-04-12-vesna'
 
 const config = {
+    appName: appName,
     apiKey: "AIzaSyAkBB770P-MOKox9k6DjFRKi9J-bIoMnJs",
     authDomain: `${appName}.firebaseapp.com`,
     databaseURL: `https://${appName}.firebaseio.com`,
