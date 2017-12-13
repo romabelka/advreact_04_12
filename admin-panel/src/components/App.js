09a6839940faad9546c5d9816a07c431c49f59f3
veranika-savanchuk <veronika.savan4uck@gmail.com>
2017-12-13 21:38:26 +0300

    HW_2_4

--- admin-panel/src/components/App.js
+++ admin-panel/src/components/App.js
@@ -4,6 +4,7 @@ import AuthPage from './routes/auth'
 import AdminPage from './routes/Admin'
 import ProtectedRoute from './common/ProtectedRoute'
 import PersonPage from './routes/PersonPage'
+import EventsPage from './routes/EventsPage'
 
 class App extends Component {
     static propTypes = {
@@ -17,10 +18,12 @@ class App extends Component {
                 <ul>
                     <li><NavLink to='/admin' activeStyle = {{color: 'red'}}>admin</NavLink></li>
                     <li><NavLink to='/people' activeStyle = {{color: 'red'}}>people</NavLink></li>
+                    <li><NavLink to='/events' activeStyle = {{color: 'red'}}>events</NavLink></li>
                 </ul>
                 <ProtectedRoute path = '/admin' component = {AdminPage}/>
                 <ProtectedRoute path="/people" component={PersonPage}/>
                 <Route path = '/auth' component = {AuthPage}/>
+                <Route path = '/events' component = {EventsPage}/>
             </div>
         )
     }
