2b83276d568c9027bbd71b2d12ca3f560da5b80f
Roman Iakobchuk <kaboom.ua@gmail.com>
2017-12-21 19:53:32 +0200

    animate Trash

--- admin-panel/src/components/events/Trash.js
+++ admin-panel/src/components/events/Trash.js
@@ -3,6 +3,7 @@ import {DropTarget} from 'react-dnd'
 import {connect} from 'react-redux'
 import {deleteEvent, stateSelector} from '../../ducks/events'
 import Loader from '../common/Loader'
+import {Motion, spring} from 'react-motion'
 
 class Trash extends Component {
     static propTypes = {
@@ -11,18 +12,28 @@ class Trash extends Component {
 
     render() {
         const {connectDropTarget, isOver, loading} = this.props
-        const style = {
+        const dndStyle = {
             border: `1px solid ${isOver ? 'green' : 'black'}`,
             width: 100, height: 100,
             position: 'fixed',
             top: 0, right: 0
         }
-        return connectDropTarget(
-            <div style = {style}>
-                Trash
-                {loading && <Loader/>}
-            </div>
-        )
+
+        return <Motion
+            defaultStyle = {{ opacity: 0 }}
+            style = {{ opacity: spring(1, {stiffness: 50, damping: 50}) }}
+            onRest = {() => console.log('---', 'ready')}
+        >
+            {style => {
+                console.log('---', style)
+                return connectDropTarget(
+                    <div style = {{...dndStyle, ...style}}>
+                        Trash
+                        {loading && <Loader/>}
+                    </div>
+                )
+            }}
+        </Motion>
     }
 }
 
