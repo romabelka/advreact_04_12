cbd2efa718b7b61a6ec621c7ff1f84fe34d9d7be
Roman Iakobchuk <kaboom.ua@gmail.com>
2017-12-21 20:04:44 +0200

    add TransitionMotion

--- admin-panel/src/components/events/SelectedEvents.js
+++ admin-panel/src/components/events/SelectedEvents.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import {connect} from 'react-redux'
 import {selectedEvents} from '../../ducks/events'
 import SelectedEventCard from './SelectedEventCard'
+import {TransitionMotion, spring} from 'react-motion'
 
 class SelectedEvents extends Component {
     static propTypes = {
@@ -12,9 +13,41 @@ class SelectedEvents extends Component {
         return (
             <div>
                 <h2>Selected Events</h2>
-                {this.props.events.map(event => <SelectedEventCard event = {event} key = {event.uid}/>)}
+                <TransitionMotion
+                    styles = {this.getStyles()}
+                    willEnter = {this.willEnter}
+                    willLeave = {this.willLeave}
+                >
+                    {interpolatedData => (
+                        <div>
+                            {interpolatedData.map(interpolated => (
+                                <div style = {interpolated.style} key = {interpolated.key}>
+                                    <SelectedEventCard event = {interpolated.data}/>
+                                </div>
+                            ))}
+                        </div>
+                    )}
+                </TransitionMotion>
             </div>
         )
+    }
+
+    willLeave = () => ({
+        opacity: spring(0, { stiffness: 20 })
+    })
+
+    willEnter = () => ({
+        opacity: 0
+    })
+
+    getStyles() {
+        return this.props.events.map(event => ({
+            key: event.uid,
+            style: {
+                opacity: spring(1, { stiffness: 40 })
+            },
+            data: event
+        }))
     }
 }
 
